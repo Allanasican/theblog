aea2e788caeab9b1297a09e3a87bcd68aa871c4e
Allanasican <allan.asican2702@gmail.com>
2023-04-04 15:25:09 +0800

    first commit

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="post">
+    <div class="container">
+        <div class="post__wrapper">
+            <h1><?php the_title(); ?> </h1>
+            <?php echo get_the_post_thumbnail(); ?>
+            <small><?php echo get_the_date('M-d-Y')?></small>
+            <p><?php the_excerpt(); ?></p>
+            <p><?php the_content(); ?></p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
